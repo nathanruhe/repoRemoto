ccdc7e96500224be11e18a2a2625d2e445336541
nathanruhe <nathan.ruhe@hotmail.com>
2024-03-19 17:22:14 +0100

    añadir proyecto node y reto 2 funciones

---
+++ run.js
@@ -0,0 +1,10 @@
+// --------------- RETO 2 ---------------
+const mylib = require("./index");
+
+console.log(mylib.suma(10, 5));
+console.log(mylib.resta(10, 5));
+console.log(mylib.producto(10, 5));
+console.log(mylib.division(10, 5));
+
+
+console.log(mylib.cuadrado(3));
